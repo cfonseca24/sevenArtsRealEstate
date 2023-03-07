7fde7e983264ed03d28286b673c70791d0a50aa0
Chris Fonseca <fonseca.j.chris@gmail.com>
2023-03-07 15:00:05 -0500

    Added an embedded map, will haave to change later.

--- client/src/pages/property-details.tsx
+++ client/src/pages/property-details.tsx
@@ -257,12 +257,13 @@ const PropertyDetails = () => {
 					</Stack>
 
 					<Stack>
-						<img
-							src="https://serpmedia.org/scigen/images/googlemaps-nyc-standard.png?crc=3787557525"
+						<iframe
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114964.53925880758!2d-80.29949842885611!3d25.782390733434813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b0a20ec8c111%3A0xff96f271ddad4f65!2sMiami%2C%20FL!5e0!3m2!1sen!2sus!4v1678219098293!5m2!1sen!2sus"
 							width="100%"
 							height={306}
 							style={{ borderRadius: 10, objectFit: "cover" }}
-						/>
+							aria-hidden="false"
+						></iframe>
 					</Stack>
 
 					<Box>
